59e85df7c3c4a0949590af53ae0f3dd60c491c46
t.nugent <t.nugent@hotmail.co.uk>
2024-01-25 10:32:26 +0000

    Setting up models

---
+++ RunGroopWebApp/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.Models
+{
+    public class Address
+    {
+        // Identifies it as a primary key
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+
+    }
+}
---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs {  get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
