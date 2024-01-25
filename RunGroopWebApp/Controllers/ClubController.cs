13cfda9a41e90004f1193013df8c0484ec551889
t.nugent <t.nugent@hotmail.co.uk>
2024-01-25 14:29:24 +0000

    Club and Race pages displaying lists from the database

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        // Dependency injection
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Immediate execution
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
