1d5714ccdc575bf0878f69e4e0660e51ae1d4356
t.nugent <t.nugent@hotmail.co.uk>
2024-01-28 13:25:03 +0000

    User can login

---
+++ RunGroopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                // User found
+                // Check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if(passwordCheck)
+                {
+                    // Password correct
+                    // Sign in
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                // Password fails
+                TempData["Error"] = "Wrong credentials. Please try again";
+                return View(loginViewModel);
+            }
+            // No existing user
+            TempData["Error"] = "Wrong credentials. Please try again";
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ RunGroopWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is requied")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
